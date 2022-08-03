eba422413c804c51d87913f1f20fc8481913dca7
alibeast8 <touaitiaaliali@gmail.com>
2022-08-03 13:16:40 +0100

    Added the color palette

--- lib/core/constant/color.dart
+++ lib/core/constant/color.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const Color primary = Color.fromARGB(255, 0, 0, 0);
+  static const bluecell = Color(0xff9395D3);
+  static const plainwhite = Color(0xffFfFfFf);
+  static const ghostwhite = Color(0xffFBF9FF);
+  static const skygrey = Color(0xff979C9E);
+  static const black = Color(0xFF050203);
+
 }
+
 
 class AppFont {
   static const String primary = "Poppins";
